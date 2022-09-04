ee4216c5995cd52d782e74ff46024286e55bd22c
crescentpartha <crescentpartha406@gmail.com>
2022-09-04 20:13:01 +0600

    Not Found page modified

--- applify-quiz/src/components/ShardPage/NotFound/NotFound.js
+++ applify-quiz/src/components/ShardPage/NotFound/NotFound.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './NotFound.css';
+import { Link } from 'react-router-dom';
+import notFound from '../../../images/notFound.jpg';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Not Found</h2>
+        <div className='d-flex flex-column gap-3 align-items-center justify-content-center m-5'>
+            <img className='landing-img' src={notFound} alt="Page Not Found" />
+            <Link className='btn btn-success' to='/home'>Back to Homepage</Link>
         </div>
     );
 };
