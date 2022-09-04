72749242d980657245be827ffcebc4cfe44b6e97
crescentpartha <crescentpartha406@gmail.com>
2022-09-04 13:28:34 +0600

    Header component modified - installed React-Bootstrap and added Bootstrap CDN Link

--- applify-quiz/src/components/ShardPage/Header/Header.js
+++ applify-quiz/src/components/ShardPage/Header/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2>Header</h2>
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="secondary" sticky='top' variant="dark">
+            <Container>
+                <Navbar.Brand as={Link} to="/home">Appify Quiz</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link className='mx-5 fw-semibold' as={Link} to="/home">Home</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link className='fw-semibold' as={Link} to="/signIn">Sign in</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
